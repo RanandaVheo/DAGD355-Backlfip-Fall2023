9d9a477705c54a71379c0d2982c4dd8e2b8d610f
Hank Kauffman <hankik26@gmail.com>
2023-09-22 21:17:43 -0400

    Messed Around with Tilesystem

--- Backlfip/Assets/Scripts_Hank/GameManager_Hank.cs
+++ Backlfip/Assets/Scripts_Hank/GameManager_Hank.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager_Hank : MonoBehaviour
 {
     [SerializeField] GameObject itemPrefab;
+    public GameObject tilePalletteHandle;
     private Timer_Hank itemSpawnTimer = new(2);
+    private Tilemap tilemap;
+
+    private void Start()
+    {
+
+        tilemap = tilePalletteHandle.GetComponentInChildren<Tilemap>();
+    }
 
     void Update()
     {
@@ -13,7 +22,8 @@ public class GameManager_Hank : MonoBehaviour
         if (itemSpawnTimer.isDone)
         {
             itemSpawnTimer.Reset();
-            Instantiate(itemPrefab, new Vector2(transform.position.x, transform.position.y), Quaternion.identity, transform.parent);
+            Instantiate(itemPrefab, tilemap.GetCellCenterWorld(new Vector3Int((int)transform.position.x, (int)transform.position.y, 0)), Quaternion.identity, transform.parent);
+            tilemap.GetCellCenterLocal(new Vector3Int(0, 0, 0));
         }
     }
 }
