e2f35186b772a116b827d95a18e33d68a9b3ced6
Hank K <hankik26@gmail.com>
2023-10-08 22:15:27 -0400

    Animations Update

--- Backlfip/Assets/Scripts_Hank/Player_Hank.cs
+++ Backlfip/Assets/Scripts_Hank/Player_Hank.cs
@@ -8,10 +8,12 @@ public class Player_Hank : MonoBehaviour
     public float speed = 1.0f;
     public float maxSpeed = 5f;
 
+    [SerializeField] private GameObject projectilePrefab;
     private Rigidbody2D rb;
     private PlayerInventory_Hank inventory;
     private Animator animator;
-    private Timer_Hank timeToStopTimer = new(.5f);
+    private Timer_Hank attackingAnimationTimer = new(.5f);
+    private Timer_Hank damagedAnimationTimer = new(.5f);
     private bool qDownLastFrame = false;
 
     // Start is called before the first frame update
@@ -25,31 +27,65 @@ public class Player_Hank : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ( true ) 
-        {
-            
-            rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0f));
-            timeToStopTimer.Reset();
-        }
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * speed, 0f));
+        
         //if (Input.GetAxis("Horizontal") == 0f)
         //{
         //    timeToStopTimer.Update();
         //    rb.velocity = new Vector2( Mathf.Lerp(rb.velocity.x, 0f, timeToStopTimer.timeLeft / timeToStopTimer.duration), rb.velocity.y);
         //}
 
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.anyKeyDown)
         {
-            if (!qDownLastFrame)
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-                DropFromInventory(inventory.TryGetItem());
+                if (!qDownLastFrame)
+                {
+                    DropFromInventory(inventory.TryGetItem());
+                }
+                qDownLastFrame = true;
+            } else
+            {
+                qDownLastFrame = false;
             }
-            qDownLastFrame = true;
-        }
-        else
-        {
-            qDownLastFrame = false;
+
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                attackingAnimationTimer.Reset();
+                animator.SetBool("IsAttacking", true);
+                GameObject tempProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity, transform);
+                tempProjectile.GetComponent<Rigidbody2D>().AddForce( Vector2.right * 100 );
+                tempProjectile.GetComponent<Rigidbody2D>().AddTorque(-1000);
+            } 
+
+            if (Input.GetMouseButton(1))
+            {
+                damagedAnimationTimer.Reset();
+                animator.SetBool("IsDamaged", true);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * 100);
+            }
+            {
+                
+            }
         }
 
+        attackingAnimationTimer.Update();
+        damagedAnimationTimer.Update();
+        if (attackingAnimationTimer.isDone)
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+        if (damagedAnimationTimer.isDone)
+        {
+            animator.SetBool("IsDamaged", false);
+        }
+
+        animator.SetBool("IsGrounded", rb.velocity.y == 0f);
         animator.SetBool("IsMoving", rb.velocity.x > 1f || rb.velocity.x < -1f);
     }
 
